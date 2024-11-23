8d9f6df0952e7c32f23106028f15dff596079d2f
M-M-Mursalin <69346389+M-M-Mursalin@users.noreply.github.com>
2024-11-23 18:30:32 +0600

    Update java.js

--- java.js
+++ java.js
@@ -6,6 +6,6 @@ console.log(resultValue);
 
 function rollDice(){
     let randomNumber = Math.floor(Math.random() * 6) + 1;
-    let imgSource = 'img/' + randomNumber + '.png';
+    let imgSource = 'new/img/' + randomNumber + '.png';
     document.getElementById('ludo').src = imgSource;
 }
